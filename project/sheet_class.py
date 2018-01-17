d46e0a9815f6c24c6cd664f42b8ecfed4473f411
JustChillin21 <JustChillin21@users.noreply.github.com>
2018-01-17 15:15:25 +0100

    Print out now working.
    date update now working
    -need to be able to adjust month and year. may already be involved though
    
    -have to try to get it to return a dictionary

--- project/sheet_class.py
+++ project/sheet_class.py
@@ -15,7 +15,10 @@ class check_time:
 
     @staticmethod
     def roundtime(x, base=6):  # Rounds time up to the nearest 6 minute interval
-        return int(math.ceil(x / base)) * base
+        if x==0:
+            return '00'
+        else:
+            return int(math.ceil(x / base)) * base
 
 
 
@@ -23,6 +26,7 @@ class check_time:
         self.time=time
         self.show=show
         self.date=datetime.strptime(date, '%Y-%m-%d')
+        self.new_time=None
 
         if type(self.time) is datetime:
             # print("We are in Checktime "+datetime.date(time, '%Y-%m-%d %H:%M'))
@@ -38,39 +42,35 @@ class check_time:
             if ntime > 23:
                 ntime -= 24
                 add_days = 1
-                stime = str(ntime) + ":" + str(check_time.roundtime(nminute))
-                if len(stime) < 5: stime = "0" + stime
-                wtime = stime
-                if self.show != 0: print(wtime)
-                # print("Change time span: {}".format(wtime))
-                to_replace=datetime.strptime(wtime, '%H:%M')
-                to_replace=to_replace.replace(year=to_update.year, month=to_update.month, day=to_update.month)
-                item = check_time.repl_date(to_replace, add_days)  ###FINISH THIS LINE
-            else:
-                item =check_time.repl_date(datetime.strptime(wtime, '%H:%M'))  ###FINISH THIS LINE
-                print('No Show')
-                pass
-            if show != 0: print('Check Time results: {} becomes {}'.format(time, item))
 
-        def __repr__(self):
-            return '{}'.format(item)
+                mntime=str(ntime)
+                if len(mntime)<2:stime[0]="0"+mntime
+
+                stime[1] = str(check_time.roundtime(nminute))
+
+                wtime = stime[0]+':'+stime[1]
+                if self.show != 0: print(wtime)
+                updated_time=datetime.strptime(wtime, '%H:%M')
+                to_replace=updated_time.replace(year=to_update.year, month=to_update.month, day=to_update.day) ##Trouble shoot for working between Years and Months
+                self.new_time = check_time.repl_date(to_replace, add_days)  ###FINISH THIS LINE
+            else:
+                self.new_time =check_time.repl_date(datetime.strptime(wtime, '%H:%M'))  ###FINISH THIS LINE
+                print(self.new_time)
+
+            if show != 0: print('Check Time results: {}{} becomes {}'.format(self.date, time, self.new_time))
+
+    def __repr__(self):
+        print(self.new_time)
+        return '{}'.format(self.new_time)
 
 
     @staticmethod
     def repl_date(item, add_days=0, show=0):
-        print("Print item {} -> {}".format(item, item.year))
+        print("Print item {} -> {}".format(item, item.day))
         new_item = item.replace(year=item.year, month=item.month, day=item.day)
         item = new_item + timedelta(days=add_days)
         if show != 0: print("Old: {} ==> New: {}".format(item, new_item))
         return item
-
-
-
-
-
-
-
-
 
 
 class Travel:
@@ -96,25 +96,35 @@ class Hours:
     def __repr__(self):
         return "Start: {} Crew: {} Wrap: {} \n[Travel] {}".format(self.start, self.crew, self.wrap, self.travel)
 
+
 class Lunch:
     def __init__(self, date=None, start=None, end=None):
         self.date = date
-        self.start=check_time(time=start, date=self.date, show=1) #save date in here as well for math
-        self.end=check_time(time=end, date=self.date) #save date in here as well for math
-        print('{}{}'.format(self.start, self.end))
+        self.start=start
+        self.end=end
         self.total_time=None
-        if self.date:
-            self.date=datetime.strptime(date, '%Y-%m-%d')
-            new_start= datetime.strptime(self.start, '%H:%M')
-            new_start = self.start.strftime(self.start, '%H:%M')
-            new_end=datetime.strptime(self.end, '%H:%M')
-            self.start=new_start.replace(year=self.date.year, month=self.date.month, day=self.date.day)
-            self.end=new_end.replace(year=self.date.year, month=self.date.month, day=self.date.day)
-            self.total_time = self.end - self.start
-            print(self.total_time)
 
-            # self.start=self.start.strftime('%H:%M')
-            # self.end=self.end.strftime('%H:%M')
+        if self.date:
+            print('Start: {} End:{} Date:{}'.format(self.start, self.end, self.date))
+            self.start = check_time(time=start, date=self.date)  # save date in here as well for math
+            self.end = check_time(time=end, date=self.date)  # save date in here as well for math
+
+            self.date=datetime.strptime(date, '%Y-%m-%d')
+            # new_start= datetime.strptime(self.start, '%H:%M')
+            self.start=self.start.new_time.replace(year=self.date.year, month=self.date.month, day=self.date.day)
+            self.end=self.end.new_time.replace(year=self.date.year, month=self.date.month, day=self.date.day)
+            ##Changing to String
+            # new_start = self.start.new_time.strftime('%H:%M')
+            new_start = self.start.strftime('%H:%M')
+            new_end=self.end.strftime('%H:%M')
+            ##Changing to String
+            # self.start=new_start.replace(year=self.date.year, month=self.date.month, day=self.date.day)
+            # self.end=new_end.replace(year=self.date.year, month=self.date.month, day=self.date.day)
+            # self.total_time = self.end.new_time - self.start.new_time
+            self.total_time = self.end - self.start
+
+            # self.start=self.start.new_time.strftime('%H:%M')
+            # self.end=self.end.new_time.strftime('%H:%M')
 
     def __repr__(self):
         if self.date:
@@ -147,18 +157,37 @@ class Sheet:
 
 
     def __repr__(self):
+        temp_sheet=[self.date, self.hours, self.meals]
+        print('\n\nResults: {}\n'.format(temp_sheet))
         return "[Date] {} \n[Hours] {} \n[Meals] {}".format(self.date, self.hours, self.meals)
 
 Date='2018-01-17'
 
-# hours=Hours()
-lunch_1=Lunch(date=Date,start='25:00', end='25:30')
+
+# lunch_1=Lunch(date=Date,start='26:15', end='26:45')
+
+lunch_1=Lunch()
 lunch_2=Lunch()
-# meals=Meals(lunch_1)
+
 travel_time=Travel(to_set=':20', from_set=':20')
-# print(travel_test)
 temp=[]
-temp.append(Sheet(Date, Hours(start='09:00', crew='09:00', wrap='26:00',travel=travel_time), Meals(lunch_1=lunch_1, lunch_2=lunch_2)))
+temp.append(Sheet(Date, Hours(start='09:00', crew='09:00', wrap='28:00',travel=travel_time), Meals(lunch_1=lunch_1, lunch_2=lunch_2)))
+
+sheet_keys = [
+    'date',
+    'start',
+    'crew_call',
+    'wrap',
+    'travel',
+    'ndb',
+    'meals'
+]
+
+time_keys = [
+    'start',
+    'end'
+]
+
 print(temp)
 print(temp[0].meals.meal_1.date)
 
