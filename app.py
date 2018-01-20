816c5be26cccbfcbc651b51b268437bc4af19899
JustChillin21 <JustChillin21@users.noreply.github.com>
2018-01-20 02:32:05 +0100

    Big Updates: time_calculations working as functions instead of classes sheet_class - all functions working. -have to try to get it to return a dictionary +Added #TODOS +Rounds values to nearest .10 of an hour

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from datetime import timedelta
 from sheet_class import TimeSheet, Lunch, Travel, Hours, Meals
-from time_calculations import mod_date
+# from time_calculations import mod_date
+import time_calculations
 
 print('Date')
-work_date=mod_date('2017-12-31')
+work_date=('2017-12-31')
 # work_date=input("Please enter the date: ")
-Date=mod_date(work_date)
+Date=time_calculations.mod_date(work_date)
 TimeSheet.date=Date
 print('Lunch 1...')
-lunch_1=Lunch(start='23:45', end='24:45')
+lunch_1=Lunch(start='13:45', end='14:15')
 print('Lunch 2...')
 lunch_2=Lunch()
 print('Travel Time...')
 travel=Travel(to_set=':20', from_set=':20')
 print('Hours...')
-hours=Hours(start='09:00', crew='09:00', wrap='28:00', travel=travel)
+hours=Hours(start='09:00', crew='09:00', wrap='18:21', travel=travel)
 # hours_start=hours.start - travel_time.to_set
 hours_start=hours.start - timedelta(hours=0,minutes=20)
 ndb=True
@@ -28,4 +29,6 @@ temp=TimeSheet(Date,hours,meals)
 sheets=[]
 sheets.append(temp)
 print(sheets[0])
-print(sheets[0].hours.total_hours-sheets[0].meal_1.total_time)
+work_time=sheets[0].hours.total_hours-sheets[0].meal_1.total_time
+print(work_time)
+print(time_calculations.converttime(work_time))
